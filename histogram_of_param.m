64efd18693f5b084cbfe4bf13ae84f7eafd4e2ca
Alec Hoyland <alec.hoyland@posteo.net>
2019-11-26 16:14:19 -0500

    Add function for plotting a semilogx histogram

---
+++ histogram_of_param.m
@@ -0,0 +1,16 @@
+function p = histogram_of_param(varargin)
+
+  if nargin == 1
+    h = figure;
+    [n, edges] = histcounts(log10(varargin{1}));
+    p = histogram(varargin{1}, 10.^edges), 'Normalization', 'countdensity';
+    set(gca, 'XScale', 'log');
+  else
+    [~, edges] = histcounts(log10(varargin{2}));
+    histogram(varargin{1}, varargin{2}, 10.^edges, 'Normalization', 'countdensity');
+    set(varargin{1}, 'XScale', 'log');
+  end
+
+  figlib.pretty();
+
+end
