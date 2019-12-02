d5d63d91c63904cdd8183756b30cac2b392cc99f
Alec Hoyland <alec.hoyland@posteo.net>
2019-12-02 15:33:06 -0500

    bleh

--- figure_1.m
+++ figure_1.m
@@ -4,48 +4,48 @@
 neuro_cosci_review_data;
 
 %% Plot histograms
-
-% intrinsic parameters
-figure
-histogram_of_param([neuro.nIntrinsicParams]);
-hold on;
-ax = gca;
-histogram_of_param([cosci.nIntrinsicParams]);
-xlabel('# of intrinsic parameters')
-legend({'Neuro', 'CoSci'}, 'Location', 'best')
-
-% synaptic parameters
-figure
-histogram_of_param([neuro.nSynapticParams]);
-hold on;
-ax = gca;
-histogram_of_param([cosci.nSynapticParams]);
-xlabel('# of synaptic parameters')
-legend({'Neuro', 'CoSci'}, 'Location', 'best')
-
-% compartments
-figure
-histogram_of_param([neuro.nCompartments]);
-xlabel('# of compartments')
-
-% neurons
-figure
-histogram_of_param([neuro.nNeurons]);
-hold on;
-ax = gca;
-histogram_of_param([cosci.nNeurons]);
-xlabel('# of neurons')
-legend({'Neuro', 'CoSci'}, 'Location', 'best')
-
-% layers
-figure
-histogram_of_param([neuro.nLayers]);
-hold on;
-ax = gca;
-histogram_of_param([cosci.nLayers]);
-xlabel('# of layers')
-legend({'Neuro', 'CoSci'}, 'Location', 'best')
-
+%
+% % intrinsic parameters
+% figure
+% histogram_of_param([neuro.nIntrinsicParams]);
+% hold on;
+% ax = gca;
+% histogram_of_param([cosci.nIntrinsicParams]);
+% xlabel('# of intrinsic parameters')
+% legend({'Neuro', 'CoSci'}, 'Location', 'best')
+%
+% % synaptic parameters
+% figure
+% histogram_of_param([neuro.nSynapticParams]);
+% hold on;
+% ax = gca;
+% histogram_of_param([cosci.nSynapticParams]);
+% xlabel('# of synaptic parameters')
+% legend({'Neuro', 'CoSci'}, 'Location', 'best')
+%
+% % compartments
+% figure
+% histogram_of_param([neuro.nCompartments]);
+% xlabel('# of compartments')
+%
+% % neurons
+% figure
+% histogram_of_param([neuro.nNeurons]);
+% hold on;
+% ax = gca;
+% histogram_of_param([cosci.nNeurons]);
+% xlabel('# of neurons')
+% legend({'Neuro', 'CoSci'}, 'Location', 'best')
+%
+% % layers
+% figure
+% histogram_of_param([neuro.nLayers]);
+% hold on;
+% ax = gca;
+% histogram_of_param([cosci.nLayers]);
+% xlabel('# of layers')
+% legend({'Neuro', 'CoSci'}, 'Location', 'best')
+%
 %% Plot # of neurons vs. # of intrinsic parameters per neuron
 
 figure; hold on
@@ -60,8 +60,8 @@ legend({'Neuro', 'CoSci'}, 'Location', 'NorthEastOutside')
 figlib.pretty('PlotBuffer', 0.2, 'AxisBox', 'on')
 xlim([0 1e6])
 ylim([0 1e6])
-% text([neuro.nNeurons] + 1, [neuro.nIntrinsicParams] ./ [neuro.nNeurons] + 0.1, ...
-    % cellstr(num2str([1:length(neuro)]')), 'FontSize', 10, 'Color', 'b');
+text([neuro.nNeurons] + 1, [neuro.nIntrinsicParams] ./ [neuro.nNeurons] + 0.1, ...
+    cellstr(num2str([1:length(neuro)]')), 'FontSize', 10, 'Color', 'k');
 axis square
 
 %% Plot # of layers vs. # of intrinsic parameters
@@ -92,6 +92,87 @@ set(gca, 'YScale', 'log')
 legend({'Neuro', 'CoSci'}, 'Location', 'NorthEastOutside')
 figlib.pretty('PlotBuffer', 0.2)
 axis square
+
+%% TEST
+
+figure; hold on;
+p(1) = scatter([neuro.nSynapticParams], [neuro.nIntrinsicParams] ./ [neuro.nNeurons]);
+p(2) = scatter([cosci.nSynapticParams], [cosci.nIntrinsicParams ]./ [cosci.nNeurons]);
+p(1).Marker = 'none';
+p(2).Marker = 'none';
+figlib.pretty('PlotBuffer', 0.2)
+text([neuro.nSynapticParams], [neuro.nIntrinsicParams] ./ [neuro.nNeurons], ...
+  cellstr(num2str([1:length(neuro)]')), 'FontSize', 10, 'Color', 'k');
+text([cosci.nSynapticParams], [cosci.nIntrinsicParams ]./ [cosci.nNeurons], ...
+  cellstr(num2str([1:length(cosci)]')), 'FontSize', 10, 'Color', 'r');
+xlabel('# of synaptic parameters')
+ylabel('# of intrinsic parameters per neuron')
+set(gca, 'XScale', 'log')
+set(gca, 'YScale', 'log')
+legend({'Neuro', 'CoSci'}, 'Location', 'NorthEastOutside')
+axis square
+
+%% TEST
+
+% generate a figure
+figure; hold on;
+cmap = colormaps.linspecer(cosci(end).label); % last number in the labels
+
+% determine which neuro models are citation duplicates
+isDuplicate = false(1, length(neuro));
+isDuplicate(2:end) = diff([neuro.label]) == 0;
+
+% plot the points
+for ii = 1:length(neuro)
+  p = scatter(neuro(ii).nSynapticParams, neuro(ii).nIntrinsicParams / neuro(ii).nNeurons, ...
+    'MarkerEdgeColor', cmap(neuro(ii).label, :), 'Marker', 'o');
+  % if a point has the same citation as the previous, turn handle visibility off
+  if isDuplicate(ii)
+    set(p, 'HandleVisibility', 'off');
+  end
+end
+
+% determine which computer science models are citation duplicates
+isDuplicate = false(1, length(cosci));
+isDuplicate(2:end) = diff([cosci.label]) == 0;
+
+% plot the points
+for ii = 1:length(cosci)
+  scatter(cosci(ii).nSynapticParams, cosci(ii).nIntrinsicParams / cosci(ii).nNeurons, ...
+    'MarkerEdgeColor', cmap(cosci(ii).label, :), 'Marker', 'x');
+  % if a point has the same citation as the previous, turn handle visibility off
+  if isDuplicate(ii)
+    set(p, 'HandleVisibility', 'off');
+  end
+end
+
+xlabel('# of synaptic parameters')
+ylabel('# of intrinsic parameters per neuron')
+set(gca, 'XScale', 'log')
+set(gca, 'YScale', 'log')
+
+% generate the legend
+legend_names = {'Destexhe et al. 1994', ...
+                'Poirazi et al. 2003', ...
+                'Izhikevich 2003', ...
+                'Hasselmo et al. 1995', ...
+                'Destexhe et al. 1996', ...
+                'Sadeh et al. 2017', ...
+                'Traub et al. 2005', ...
+                'Potjans and Diesmann 2005', ...
+                'Nadim et al. 1998', ...
+                'Lytton et al. 1997', ...
+                'Hill et al. 2002', ...
+                'Bartos et al. 2002', ...
+                'Wang et al. 1996', ...
+                'Markram et al. 2015', ...
+                'Krizhevsky et al. 2017', ...
+                'Simonyan et al. 2015', ...
+                'He et al. 2015'};
+legend(legend_names, 'Location', 'NorthEastOutside')
+
+figlib.pretty('PlotBuffer', 0.2)
+
 
 %% Plot pairwise scatter plots
 
